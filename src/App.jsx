74745226f659c2f7b3db331d185b6bee7620b179
Daniele Maselli <danymase@gmail.com>
2024-01-06 12:37:57 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello World<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
