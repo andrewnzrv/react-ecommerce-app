961bca66123175875eb936568625ccd7e060a131
Daniele Maselli <danymase@gmail.com>
2024-01-09 14:42:04 +0100

    added filter

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "@mantine/core/styles.css";
+import { Button } from "@mantine/core";
 
 
 const API_URL = 'https://api.escuelajs.co/api/v1/products'
+const API_URL_CATEGORY = 'https://api.escuelajs.co/api/v1/categories'
 
 const Product = () => {
     const [product, setProduct] = useState([])
-   
+    const [categories, setCategories] = useState([]);
+    const [filter, setFilter] = useState([]);
+
     const fetchProduct = async () => {
         try {
         const response = await fetch(`${API_URL}`)
@@ -21,28 +26,60 @@ const Product = () => {
         }       
     }
 
+    const fetchCategory = async () => {
+        try {
+        const response = await fetch(`${API_URL_CATEGORY}`)
+        //console.log(response)
+        if (response.ok) {
+            const filterData = await response.json()
+            //access the data
+            setCategories(filterData)
+            console.log(filterData)
+        }
+        } catch (error) {
+            console.log(error)
+        }       
+    }
+
     useEffect(() => { 
         fetchProduct()
+        fetchCategory()
     },[]) 
 
+    const filterItems = (catItem) => {
+        const updatedItems = product.filter((curItem) => curItem.category.name === catItem);
+        setFilter(updatedItems);
+    };
+ 
     return (
     <> 
- 
+     
         <h1 className="main-title">Our Products</h1>
-        <ul className="grid">   
-            {product.map(products => (
-                <div key={products.id}> 
-                <Link to={`/products/${products.id}`}>
-                    <img className="img" src={products.images[0]} alt={products.title}/> 
-                        <div className="product-info">      
-                            <h1>{products.title}</h1>
-                            <p>{products.price} <span>&#8364;</span> </p>         
-                        </div>  
-                </Link>    
-                                                    
-                </div>                   
-            ))}
-        </ul>
+        <div className="filter-ctn"> 
+            <ul>
+                {categories.map((cat) => (
+                <div className="filter-button" key={cat.id}>
+                    <Button variant="outline" color="gray" onClick={() => filterItems(cat.name)}>{cat.name}</Button>
+                </div>
+                ))}
+            </ul>
+    </div> 
+        <ul className="grid">
+            
+        {(filter.length > 0 ? filter : product).map((products) => (
+          <div key={products.id}>
+            <Link to={`/products/${products.id}`}>
+              <img className="img" src={products.images[0]} alt={products.title} />
+              <div className="product-info">
+                <h1>{products.title}</h1>
+                <p>
+                  {products.price} <span>&#8364;</span>{" "}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </ul>
   
     </>   
     )    
